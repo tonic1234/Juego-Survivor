e20a5258cf70a560de1c09998fa7d1abb373435d
Antonio Rama <you@example.com>
2022-09-06 15:35:19 -0300

    estado de animo, sed , mochila y efectos aregagados

--- script_v2.js
+++ script_v2.js
@@ -13,56 +13,61 @@ const esperarB = document.querySelector("#esperarButton")
 
 const antorcha = {
     nombre: "Antorcha",
-    
+
 }
 
 const hoja = {
     nombre: "Hoja",
-    
+    imagen: `<img src="./imagenes/hoja.png" alt="">`
+
 }
 const palo = {
     nombre: "Palo",
-    
+    imagen: `<img src="./imagenes/palo.png" alt="">`
 }
 const piedra = {
     nombre: "Piedra",
-    
+    imagen: `<img src="./imagenes/piedra.png" alt="">`
 }
 const botella = {
     nombre: "Botella",
-   
+    imagen: `<img src="./imagenes/botella.png" alt="">`
 }
 const fruta = {
     nombre: "Fruta Silvestre",
     valNut: 15,
-    moral: 10
+    moral: 10,
+    imagen: `<img src="./imagenes/fruta.png" alt="">`
 }
 const gusano = {
     nombre: "Gusano",
     valNut: 2,
-    moral: 10
+    moral: 10,
+    imagen: `<img src="./imagenes/gusano.png" alt="">`
 }
 const pajaro = {
-    nombre: "Pajaro",
-    
+    nombre: "Pájaro",
+    imagen: `<img src="./imagenes/pajaro.png" alt="">`
+
 }
 const carnePajaro = {
-    nombre: "Carne Cocida de Pajaro",
-    moral: 30
+    nombre: "Carne Cocida de Pájaro",
+    moral: 30,
+    valNut: 12,
+    imagen: `<img src="./imagenes/pajarococinado.png" alt="">`
 }
 
 // Arreglo de la mochila del jugador.
 
-const mochila = [ hoja, hoja, fruta, fruta, fruta]
+const mochila = [botella]
 
 // Arreglo de los objetos que hay en el juego.
 
-const objetos = [palo, fruta, gusano, hoja]
+const objetos = [palo, piedra, fruta, gusano, hoja, pajaro, botella]
 
-// Arreglo usado para la recolección y esta formado por
-// los elemento que se recolectan en cada iteración.
+// Arreglo de los objetos fabricables.
 
-const recoleccion = []
+const fabricables = [antorcha, carnePajaro]
 
 // Arreglo de frases para la pantalla de espera .
 
@@ -77,14 +82,23 @@ const frasesDeEspera = ["Te sientas bajo un árbol a esperar que pasen algunas h
 
 // Stats del personaje
 
-let salud = 50
-let hambre = 70
-let cansancio = 80
+let salud = 70
+let hambre = 40
+let sed = 60
+let cansancio = 25
+let moral = 60
+
 
 // Variables de dia y hora cada actividad lleva determinada cantidad de horas y se van sumando.
 
-let hora = 16
+let hora = 12
 let dias = 0
+
+
+// Arreglo usado para la recolección y esta formado por
+// los elemento que se recolectan en cada iteración.
+
+let recoleccion = []
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>FUNCIONES>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -99,10 +113,58 @@ function state() {
         salud -= 10
     }
 
-    if (cansancio > 100) {
-        alert("Estas muy cansado te quedaste dormido")
-        dormir()
+    if (sed > 100) {
+        salud -= 20
+    }
 
+    if (cansancio > 80) {
+        alert("Estas muy cansado te estas quedando  dormido")
+       
+    }
+    if (cansancio > 100) {
+        dormir()
+    }
+
+    if(moral > 100){
+        moral = 100
+    }
+    if(moral < 0){
+        moral = 0
+    }
+    if(cansancio < 0){
+        cansancio = 100
+    }
+  
+    if(hambre > 100){
+        hambre = 100
+    }
+    if(hambre < 0){
+        hambre = 0
+    }
+
+    if(sed > 100){
+        sed = 100
+    }
+    if(sed < 0){
+        sed = 0
+    }
+    if (salud > 100){
+        salud = 100
+    }
+
+  
+    if (moral > 40) {
+        document.querySelector(".gameBox").style.filter = `grayscale(0%)`;
+    }
+
+    if (moral <= 40) {
+        document.querySelector(".gameBox").style.filter = `grayscale(50%)`;
+    }
+    if (moral <= 35) {
+        document.querySelector(".gameBox").style.filter = `grayscale(75%)`;
+    }
+    if (moral <= 20) {
+        document.querySelector(".gameBox").style.filter = `grayscale(100%)`;
     }
 }
 
@@ -110,6 +172,8 @@ function state() {
 // Función del reloj
 
 function clock() {
+
+    
     if (hora == 24) {
         hora = 0
         dias += 1
@@ -124,15 +188,13 @@ function clock() {
         document.querySelector(".timeText").innerHTML = hora + ":00 hs";
         document.querySelector(".dayText").innerHTML = "Día: " + dias;
         return
-    }
-    else if(hora == 24){
+    } else if (hora == 24) {
         hora = 0
         dias += 1
         document.querySelector(".timeText").innerHTML = hora + ":00 hs";
         document.querySelector(".dayText").innerHTML = "Día: " + dias;
         return
-    }
-    else{
+    } else {
         document.querySelector(".timeText").innerHTML = hora + ":00 hs";
         document.querySelector(".dayText").innerHTML = "Día: " + dias;
     }
@@ -144,35 +206,24 @@ function clock() {
 
 function sunMoon() {
     let hourDivition = hora * 15
-    let hourV = -(hourDivition) - 180
+    let hourV = -(hourDivition) + 180
     document.querySelector(".sunMoonImg").style.transform = 'rotate(' + hourV + 'deg)';
-
-}
-
-// Función del campamento. Se hace de dia y de noche según la hora
-
-function camping() {
-    if (hora >= 6 && hora <= 17) {
-        document.querySelector(".camping").style.backgroundImage = "url(../imagenes/campingdia.png)";
-    } else if (hora >= 19 && hora < 6) {
-        document.querySelector(".camping").style.backgroundImage = "url(../imagenes/campingnoche.png)";
-    } else {
-        document.querySelector(".sky").style.background = "linear-gradient(0deg, rgba(8,81,204,1) 0%, rgba(3,16,34,1) 100%)";
-
-    }
 
 }
 
 // Función del cielo. cambia el degradado del DOM según la hora
 
 function dayNight() {
-    if (hora >= 6 && hora <= 17) {
-        document.querySelector(".sky").style.background = "linear-gradient(0deg, rgba(255, 255, 255, 1) 0%, rgb(87, 157, 255) 100%)";
-    } else if (hora >= 19 && hora < 6) {
-        document.querySelector(".sky").style.background = "linear-gradient(0deg, rgba(255,112,45,1) 0%, rgba(252,176,69,1) 100%)";
-    } else {
-        document.querySelector(".sky").style.background = "linear-gradient(0deg, rgba(8,81,204,1) 0%, rgba(3,16,34,1) 100%)";
+    if (hora >= 6 && hora < 17) {
+        document.querySelector(".sky").style.backgroundImage = "url(../imagenes/cielodia.png)";
+        document.querySelector(".camping").style.backgroundImage = "url(../imagenes/campingdia.png)"
 
+    } else if (hora >= 17 && hora < 19) {
+        document.querySelector(".sky").style.backgroundImage = "url(../imagenes/cielotarde.png)";
+        document.querySelector(".camping").style.backgroundImage = "url(../imagenes/campingdia.png)"
+    } else {
+        document.querySelector(".sky").style.backgroundImage = "url(../imagenes/cielonoche2.png)";
+        document.querySelector(".camping").style.backgroundImage = "url(../imagenes/campingnoche.png)"
     }
 
 }
@@ -199,6 +250,8 @@ function comer() {
         hambre -= fruta.valNut
         cansancio += 20
         hora += 1
+        sed += 20
+        moral += 10
 
         //   Si hay frutas en el arreglo mochila la fruta tiene que Ser
         //   eliminada de mi arreglo cuando el personaje la coma
@@ -206,8 +259,8 @@ function comer() {
         // 1-filtro todo los elementos que no son fruta y creo el arreglo filtrados
         // 2-filtro todos los elemento fruta en el arreglo no filtrados
 
-        const filtrados = mochila.filter(element => element != fruta)
-        const noFiltrados = mochila.filter(element => element == fruta)
+        let filtrados = mochila.filter(element => element != fruta)
+        let noFiltrados = mochila.filter(element => element == fruta)
 
         // Vació el arreglo mochila
 
@@ -226,13 +279,17 @@ function comer() {
         for (let index = 0; index < noFiltrados.length - 1; index++) {
             mochila.push(fruta)
         }
+
+
         clock()
         state()
         statusBar(".statusBarHealth", salud)
         statusBar(".statusBarFatigue", cansancio)
         statusBar(".statusBarHunger", hambre)
+        statusBar(".statusBarThirst", sed)
+        statusBar(".statusBarMood", moral)
+        inventario()
         dayNight()
-        camping()
         sunMoon()
 
     } else {
@@ -241,6 +298,63 @@ function comer() {
     }
 }
 
+// Función tomar agua
+
+function tomarAgua() {
+
+    if (sed <= 20) {
+        alert("No puedes beber agua, no tienes sed")
+    } else if (mochila.some(bebida => bebida.nombre == "Botella") === true) {
+        sed -= 30
+        cansancio -= 10
+        hora += 1
+        moral += 10
+
+        //   Si hay botella en el arreglo mochila la botella tiene que Ser
+        //   eliminada de mi arreglo cuando el personaje la coma
+
+        // 1-filtro todo los elementos que no son fruta y creo el arreglo filtrados
+        // 2-filtro todos los elemento fruta en el arreglo no filtrados
+
+        let filtrados = mochila.filter(element => element != botella)
+        let noFiltrados = mochila.filter(element => element == botella)
+
+        // Vació el arreglo mochila
+
+        mochila.length = 0;
+
+        // Pongo los elementos del arreglo filtrados dentro de arreglo mochila
+
+        Array.prototype.push.apply(mochila, filtrados);
+
+        // Filter nos saco todos los elemento botella del arreglo mochila pero el personaje
+        // puede tener mas de una botella en el inventario
+        // Con el arreglo noFiltrados sabemos cuantas botellas tenia el arreglo mochila
+        // Con ciclo for los volvemos a poner dentro de la mochila la cantidad de veces que existiera
+        // el elemento fruta en el arreglo - 1 que seria la fruta que el personaje comió
+
+        for (let index = 0; index < noFiltrados.length - 1; index++) {
+            mochila.push(botella)
+        }
+
+
+        clock()
+        state()
+        statusBar(".statusBarHealth", salud)
+        statusBar(".statusBarFatigue", cansancio)
+        statusBar(".statusBarHunger", hambre)
+        statusBar(".statusBarThirst", sed)
+        statusBar(".statusBarMood", moral)
+        inventario()
+        dayNight()
+        sunMoon()
+    }
+}
+
+
+
+
+
 // Función de la acción dormir .
 
 function dormir() {
@@ -248,18 +362,24 @@ function dormir() {
         alert("No puedes dormir, no tienes sueño.")
 
     } else {
-        cansancio -= 30
         hambre += 20
-        hora += 6
+        sed += 20
+        cansancio -= 40
+        moral += 5
+        hora += 5
+
+
+
         clock()
         state()
         statusBar(".statusBarHealth", salud)
         statusBar(".statusBarFatigue", cansancio)
         statusBar(".statusBarHunger", hambre)
+        statusBar(".statusBarThirst", sed)
+        statusBar(".statusBarMood", moral)
+        inventario()
         dayNight()
-        camping()
         sunMoon()
-
 
     }
 }
@@ -268,34 +388,39 @@ function dormir() {
 
 function esperar() {
     hora += 3
-    cansancio += 30
+    sed += 10
+    cansancio += 10
     hambre += 20
-    // let frase = Math.ceil(Math.random() * frasesDeEspera.length - 1);
-    // alert(frasesDeEspera[frase])
+    moral -= 20
+
+    let frase = Math.ceil(Math.random() * frasesDeEspera.length - 1);
+    alert(frasesDeEspera[frase])
+
     clock()
     state()
     statusBar(".statusBarHealth", salud)
     statusBar(".statusBarFatigue", cansancio)
     statusBar(".statusBarHunger", hambre)
+    statusBar(".statusBarThirst", sed)
+    statusBar(".statusBarMood", moral)
+    inventario()
     dayNight()
-    camping()
     sunMoon()
 }
 
 // Función de la acción inventario .
 
 
-// function inventario() {
-//     if (mochila.length === 0) {
-//         alert("Tu mochila esta vacía. Sal a recolectar a ver qué puedes encontrar.")
+function inventario() {
+    mochila.sort()
+    let imagenObjeto = mochila.map((element) => element.imagen)
+    document.querySelector(".backPackimg").innerHTML = imagenObjeto
 
-//     } else {
 
-//         mochila.sort()
-//         let nombresMochila = mochila.map((element) => element.nombre)
-//         alert(nombresMochila)
-//     }
-// }
+    // let nombresMochila = mochila.map((element) => element.nombre)
+
+
+}
 
 // Función de la acción recolectar .
 
@@ -304,9 +429,11 @@ function recolectar() {
     // Solo se puede recolectar si es de dia
 
     if (hora > 6 && hora < 19 || mochila.some(element => element.nombre == "Antorcha") === true) {
-        cansancio += 30
-        hambre += 10
-        hora += 4
+        cansancio += 40
+        sed += 20
+        hambre += 20
+        hora += 2
+        
 
         recoleccion = []
 
@@ -322,21 +449,26 @@ function recolectar() {
 
             mochila.push(objetos[chance])
             recoleccion.push(objetos[chance])
+            moral += 1
         }
 
 
-        // recoleccion.sort()
-        // let nombresRecoleccion = recoleccion.map((element) => element.nombre)
-        // alert("Recolectaste esto: " + nombresRecoleccion)
-        // alert("Se agregaron a tu mochila.")
+        recoleccion.sort()
+        let nombresRecoleccion = recoleccion.map((element) => element.nombre)
+        alert("Recolectaste esto: " + nombresRecoleccion)
+        alert("Se agregaron a tu mochila.")
+
+
 
         clock()
         state()
         statusBar(".statusBarHealth", salud)
         statusBar(".statusBarFatigue", cansancio)
         statusBar(".statusBarHunger", hambre)
+        statusBar(".statusBarThirst", sed)
+        statusBar(".statusBarMood", moral)
+        inventario()
         dayNight()
-        camping()
         sunMoon()
 
     } else {
@@ -370,6 +502,23 @@ function fabricar() {
             alert("fabricaste una antorcha")
 
             mochila.push(antorcha)
+            cansancio += 30
+            sed += 30
+            hambre += 20
+            hora += 2
+            moral += 20
+
+            clock()
+            state()
+            statusBar(".statusBarHealth", salud)
+            statusBar(".statusBarFatigue", cansancio)
+            statusBar(".statusBarHunger", hambre)
+            statusBar(".statusBarThirst", sed)
+            statusBar(".statusBarMood", moral)
+            inventario()
+            dayNight()
+            sunMoon()
+
 
             const noFiltrados = mochila.filter(element => element != hoja && element != palo)
             const hojasFiltradas = mochila.filter(element => element == hoja)
@@ -410,13 +559,17 @@ function fabricar() {
 
 alert("¿Qué quieres hacer?")
 
+
+
 clock()
 state()
 statusBar(".statusBarHealth", salud)
 statusBar(".statusBarFatigue", cansancio)
 statusBar(".statusBarHunger", hambre)
+statusBar(".statusBarThirst", sed)
+statusBar(".statusBarMood", moral)
+inventario()
 dayNight()
-camping()
 sunMoon()
 
 
@@ -425,6 +578,9 @@ sunMoon()
 
 comerB.addEventListener('click', function () {
     comer();
+});
+tomarB.addEventListener('click', function () {
+    tomarAgua();
 });
 dormirB.addEventListener('click', function () {
     dormir();
